bf2ee1a291dc709f4721e1c5f0a7328a1acfee0a
hamish3307 <345943314@qq.com>
2020-03-13 20:55:57 +0800

    UserQueryDTO进行序列换

--- Api/src/main/java/cn/hamish/api/dto/UserQueryDTO.java
+++ Api/src/main/java/cn/hamish/api/dto/UserQueryDTO.java
@@ -2,14 +2,17 @@ package cn.hamish.api.dto;
 
 import lombok.Data;
 
+import java.io.Serializable;
+
 /**
  * @描述: 用户查询DTO
  * @Date: 2020-03-12 14:40
  * @Author: hha
  */
 @Data
-public class UserQueryDTO {
+public class UserQueryDTO implements Serializable {
 
+    private static final long serialVersionUID = -1930827402317065577L;
     /* ID */
     private String id;
 
